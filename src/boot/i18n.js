6921041aa9f0ecc2044ae76d7056f48a6f4a3e11
nikita <nikita45454@gmail.com>
2022-01-25 16:46:13 +0300

    feat: добавляет смену языка для полей формы

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -2,6 +2,10 @@ import { Quasar } from "quasar";
 import { createI18n } from "vue-i18n";
 import messages from "src/i18n";
 import api, { MainApi, testApi } from "src/api/utilities/service";
+import { configure } from "vee-validate";
+import { localize, setLocale as VeeSetLocale } from "@vee-validate/i18n";
+import ru from "@vee-validate/i18n/dist/locale/ru.json";
+import en from "@vee-validate/i18n/dist/locale/en.json";
 
 export default ({ app }) => {
   let curLocale;
@@ -14,6 +18,7 @@ export default ({ app }) => {
   }
 
   const i18n = createI18n({
+    silentFallbackWarn: true,
     locale: curLocale,
     fallbackLocale: "ru-RU",
     messages,
@@ -21,16 +26,25 @@ export default ({ app }) => {
 
   app.use(i18n);
 
+  // создает сообщения для правил на русском
+  configure({
+    generateMessage: localize({ ru, en }),
+  });
+
   setLocale(curLocale);
 
   app.config.globalProperties.$setLocale = setLocale;
-  window.setLocale = setLocale;
+
+  if (!window.app) window.app = {};
+  window.app.setLocale = setLocale;
 
   function setLocale(lang, reload = false) {
     i18n.global.locale = lang;
     window.localStorage.setItem("locale", lang);
 
     const serverLocale = lang.slice(0, 2);
+
+    VeeSetLocale(serverLocale);
     api.setLang(serverLocale);
     MainApi.setLang(serverLocale);
     testApi.setLang(serverLocale);
--- src/boot/registr-validators.js
+++ src/boot/registr-validators.js
@@ -1,15 +1,5 @@
-import { defineRule, configure } from "vee-validate";
+import { defineRule } from "vee-validate";
 import { required, email, min } from "@vee-validate/rules";
-import { localize, setLocale } from "@vee-validate/i18n";
-import ru from "@vee-validate/i18n/dist/locale/ru.json";
-
-// создает сообщения для правил на русском
-configure({
-  generateMessage: localize({ ru }),
-});
-
-// переключает режим отображения ошибок на русский
-setLocale("ru");
 
 // function cellphone(value) {}
 
